762c7476996608a6090fbd5e8f9a3c633bde1e3f
leylasoltani <67321179+leylasoltani@users.noreply.github.com>
2020-06-26 16:03:42 +0430

    chnaged it a little

--- xtime.c
+++ xtime.c
@@ -6,12 +6,18 @@
 int main (int argc,char *argv[])
 {
 
-	
-  int status, a = 0, b = 0;	
-  fork();
-	
-  status = waitx(&a, &b);
- 	  
-  printf(1, "Wait Time = %d\n Run Time = %d with Status %d \n", a, b, status); 
-  exit();
+ int pid;
+ int status=-1,a=3,b=4;	
+ pid = fork ();
+ if (pid == 0)
+   {	
+   exec(argv[1],argv);
+    printf(1, "exec %s failed\n", argv[1]);
+    }
+  else
+ {
+    status=waitx(&a,&b);
+ }  
+ printf(1, "Wait Time = %d\n Run Time = %d with PID %d \n",a,b,status); 
+ exit();
 }
