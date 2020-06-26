6e402cdd7c41266300822280b2fec42e9f0c5fe0
leylasoltani <67321179+leylasoltani@users.noreply.github.com>
2020-06-26 14:56:22 +0430

    fixed a bug!

--- xtime.c
+++ xtime.c
@@ -6,12 +6,12 @@
 int main (int argc,char *argv[])
 {
 
-	int pid;
-	int status, a = 0, b = 0;	
+	
+  int status, a = 0, b = 0;	
   fork();
 	
   status = waitx(&a, &b);
  	  
- 	printf(1, "Wait Time = %d\n Run Time = %d with Status %d \n", a, b, status); 
- 	exit();
+  printf(1, "Wait Time = %d\n Run Time = %d with Status %d \n", a, b, status); 
+  exit();
 }
