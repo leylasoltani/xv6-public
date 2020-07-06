110088f491529fa6d2753d5939a535374da49f02
leylasoltani <67321179+leylasoltani@users.noreply.github.com>
2020-07-06 09:18:34 +0430

    finished phase2

--- user.h
+++ user.h
@@ -24,6 +24,8 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 int waitx(int *wtime, int *rtime);
+int setpr(int pid, int priority);
+int cps(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
